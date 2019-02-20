8b39e924fd17e3511c33a71c537740e0ff7f6e11
Tennessium <mighty.igor@yandex.ru>
2019-02-20 17:23:28 +0300

    Пояснения, ууу!

--- jgen.py
+++ jgen.py
@@ -1,6 +1,6 @@
 from graffiti_drone import path
 
 heart = path.Path(shift=(0,0.9), wall_dimensions=(1.5,1.5))
-heart.load_gcode("gcode/heart.nc")
-heart.write_json("json/new.json")
-heart.plot("images/new.png")
+heart.load_gcode("gcode/heart.nc") # путь к файлу с gcode
+heart.write_json("json/new.json") # путь к файлу с координатами
+heart.plot("images/new.png") # путь к файлу с визуальным представлением координат
